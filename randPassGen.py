24792c39560b1d9182537c11ee651ca0f427876b
Octavio Frattura <ofrattura@alumno.huergo.edu.ar>
2020-10-13 19:02:21 -0300

    cambio en la generacion de contraseña

--- randPassGen.py
+++ randPassGen.py
@@ -4,7 +4,5 @@ import random
 caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 
 long = int(input('Introduzca la longitud de la contraseña:'))
-contraseña = ''
-for c in range(long):
-    contraseña += random.choice(caracteres)
+contraseña = ''.join([random.choice(caracteres) for _ in range(long)])
 print(contraseña)
