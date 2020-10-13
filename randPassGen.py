24e0e42f3b5c2f51862cdcb19514046480459c17
Tixend <63377023+Tixend@users.noreply.github.com>
2020-10-13 18:56:27 -0300

    asdasd

--- randPassGen.py
+++ randPassGen.py
@@ -1,10 +1,10 @@
 import random
 
 
-caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 
-long = int(input('Introduzca la longitud de la contraseña:'))
-contraseña = ''
-for c in range(long):
-    contraseña += random.choice(caracteres)
-print(contraseña)
+largo = int(input('Introduzca la longitud de la contraseña:'))
+passw = ''
+for c in range(largo):
+    passw += random.choice(char)
+print(passw)
