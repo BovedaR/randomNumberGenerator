a320e425001eb1133b325318d4aaa598102cd486
Octavio Frattura <ofrattura@alumno.huergo.edu.ar>
2020-10-14 11:17:25 -0300

    OneLine

--- randPassGen.py
+++ randPassGen.py
@@ -1,7 +1,2 @@
 import random
-
-caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
-
-long = int(input('Introduzca la longitud de la contraseña:'))
-contraseña = ''.join([random.choice(caracteres) for _ in range(long)])
-print(contraseña)
+print(''.join([random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789') for _ in range(int(input('Introduzca la longitud de la contraseña:')))]))
